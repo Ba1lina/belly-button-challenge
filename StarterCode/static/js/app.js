599145089244bd99aa4ac682fbe854fd9880a398
Ba1lina <105493270+Ba1lina@users.noreply.github.com>
2022-10-24 20:23:48 +1100

    Update app.js

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -4,7 +4,18 @@ const bbdata = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classro
 // Fetch the JSON data and console log it
 d3.json(bbdata).then(function(data) {
   console.log(data);
+
+// Use the JSON Data to create some constant variables
+  const id = data.names;
+    id.forEach((sample) => {
+      selector.append("option")
+      .text(sample)
+      .property("value", sample);
+      });
+    let starter = id[0];
+    metadata(starter);
+    console.log(starter);
 });
 
 // Testing with Sample ID of 940
-console.log(data);
+console.log(id[0]);
